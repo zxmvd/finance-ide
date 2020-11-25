a8e4015d0d2505cff57d67afdcf3305c1c157b39
zxmvd <zx_mailhot@hotmail.com>
2020-11-25 12:58:04 +1100

    update readme

--- helpers.py
+++ helpers.py
@@ -43,7 +43,7 @@ def lookup(symbol):
 
     # Contact API
     try:
-        api_key = "pk_cb8187c1516c4a35a1a3b49a98fd045b"
+        api_key = os.environ.get("API_KEY")
         response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
